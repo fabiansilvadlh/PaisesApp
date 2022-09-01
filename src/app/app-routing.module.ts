fe3640829bf52e4608715102c264e4554c38b4f0
Fabian Silva <fabiansilvadlh@gmail.com>
2022-09-01 01:09:20 -0400

    paises app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent
+    },
+    {
+        path: 'pais/:id',
+        component: VerPaisComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''     //acá puedo poner un component que me direccione a una página de error personalizada, si el usuario elige una que no tiene nada asignado. Así como está redirecciona a la ruta principal.
+    }
+]
+
+@NgModule({
+    imports: [ CommonModule,
+    RouterModule.forRoot( routes) ],
+    exports: [
+        RouterModule
+    ],
+})
+export class AppRoutingModule {}
